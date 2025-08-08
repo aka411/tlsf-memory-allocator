339bb26d79ad19ce279e6c552ea8f9aa61ffa493
aka411 <181660723+aka411@users.noreply.github.com>
2025-08-08 19:06:33 +0530

    feature : add header struct , add footer struct , add layout struct , add calculate layout method and implementation , add tlsf block diagram
    added the tlsf block header struct ,the tlsf footer struct.
    
    Added layout struct for  making it easier to  place the header ,footer ,padding  easily.
    
    Added a calculate layout method to calculate layout given a size and start address.

--- include/tlsf.h
+++ include/tlsf.h
@@ -1,7 +1,80 @@
 #pragma once
+#include <cstddef>
+
+struct TlsfBlockHeader
+{
+
+	/*******WARNING: 	
+	
+	The header may be offset from the raw block start address to ensure proper alignment.
+	Make sure to account for this when calculating the size of the block.
+
+	ie. The address of Header may not be same as the start address of the block.
+
+	*******/
+
+	
+
+
+	TlsfBlockHeader* next; // Pointer to the next block in the free list
+	TlsfBlockHeader* prev; // Pointer to the previous block in the free list
+	
+
+	size_t size; // Size of user space, not including the header and footer and padding
+
+	//since header also has alignment requirements, we need to ensure that the header is aligned properly
+	
+	bool isFree; // Flag to indicate if the block is free or allocated
+
+};
+
+
+
+
+struct TlsfFooter
+{
+
+	size_t size; // Size of the block, including header and footer
+
+};
+
+
+struct Layout
+{
+	size_t rawStartAddress;
+	size_t paddingHeader;
+	size_t HeaderStartAddress; // Address of the TlsfBlockHeader(INCLUSIVE)
+	size_t userAreaStartAddress; // Start address of the user area(INCLUSIVE)
+	size_t userAreaExclusiveEnd; // End address of the user area
+	size_t paddingFooter; // Padding after the user area, if any
+	size_t FooterStartAddress; // Address of the TlsfBlockFooter(INCLUSIVE)
+	size_t rawExclusiveEndAddress;
+};
+
+
+
+/*
+
+Block Header Layout:
++--------------------------------------------------------------------------------------------------------------------+
+| Padding(if any) |  TlsfBlockHeader    |    User Area     |     Padding (if any)    |    TlsfBlockFooter(size_t)    |
++--------------------------------------------------------------------------------------------------------------------+
+
+*/
 
 class TlsfAllocator
 {
+private:
+
+	const size_t BLOCK_HEADER_SIZE = sizeof(TlsfBlockHeader);
+	const uint8_t ALIGNMENT_REQ_BLOCK_HEADER = alignof(TlsfBlockHeader);
+
+	const uint8_t FOOTER_SIZE = sizeof(TlsfFooter);
+	const uint8_t ALIGNMENT_REQ_FOOTER = alignof(TlsfFooter);
+
+
+	Layout calculateLayout(void* ptr, size_t size) const;
+
 public:
 
 	void* allocate(size_t size);
--- src/tlsf.cpp
+++ src/tlsf.cpp
@@ -1,4 +1,6 @@
 
+#include "tlsf.h"
+
 
 
 void* TlsfAllocator::allocate(size_t size)
@@ -8,5 +10,42 @@ void* TlsfAllocator::allocate(size_t size)
 
 void TlsfAllocator::deallocate(void* ptr)
 {
+	//get header
+	//see if it can be collased
+	//if not store in free list
+}
 
+
+Layout TlsfAllocator::calculateLayout(void* ptr, size_t size) const
+{
+	Layout layout;
+
+	// range is always defined as [start, end)
+
+
+	// Calculate the raw start address
+	layout.rawStartAddress = reinterpret_cast<size_t>(ptr);
+
+	// Calculate padding for header
+	layout.paddingHeader = (ALIGNMENT_REQ_BLOCK_HEADER - (layout.rawStartAddress % ALIGNMENT_REQ_BLOCK_HEADER)) % ALIGNMENT_REQ_BLOCK_HEADER;
+
+	// Header start address
+	layout.HeaderStartAddress = layout.rawStartAddress + layout.paddingHeader;
+
+	// User area start address
+	layout.userAreaStartAddress = layout.HeaderStartAddress + BLOCK_HEADER_SIZE;
+
+	// User area end address
+	layout.userAreaExclusiveEnd = layout.userAreaStartAddress + size;
+
+	// Padding after user area
+	layout.paddingFooter = (ALIGNMENT_REQ_FOOTER - ((layout.userAreaExclusiveEnd + FOOTER_SIZE) % ALIGNMENT_REQ_FOOTER)) % ALIGNMENT_REQ_FOOTER;
+
+	// Footer start address
+	layout.FooterStartAddress = layout.userAreaExclusiveEnd + layout.paddingFooter;
+
+	// Raw end address
+	layout.rawExclusiveEndAddress = layout.FooterStartAddress + FOOTER_SIZE;
+
+	return layout;
 }
